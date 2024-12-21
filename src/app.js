7be53617c142db4bff864ffb0e9fedef3b775ab1
Gaurav singh <gauravbora2273@gmail.com>
2024-12-21 14:53:51 +0530

    add app.use

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }));
-
+app.use(express.json({limit:"16kb"}));   // this is for form data
+app.use(express.urlencoded({extended:true,limit:"16kb"}));  // this is for url incoding.
+app.use(express.static("pubic"))
 
 
 
